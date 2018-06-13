63f4cd8f7fcccaeb7d74fe99e2c2c8d5734ca6f8
Manel <yo@manelperez.com>
2018-06-13 09:49:42 +0200

    Empty structure with two activities and a Student model

--- app/src/main/java/es/pue/android/student/MainActivity.java
+++ app/src/main/java/es/pue/android/student/MainActivity.java
@@ -1,13 +1,33 @@
 package es.pue.android.student;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import es.pue.android.student.model.Student;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void buildStudent() {
+        student = new Student("Elon", "Musk", 35);
+    }
+
+    private void saveStudent() {
+
+    }
+
+    public void seeStudent(View view) {
+        Intent i = new Intent(this, DetailActivity.class);
+
+        startActivity(i);
     }
 }
